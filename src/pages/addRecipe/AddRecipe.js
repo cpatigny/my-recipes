6ee71f1df527b2a4375c625dbd795964ee09242e
cpatigny <63152989+cpatigny@users.noreply.github.com>
2021-10-15 20:21:39 +0200

    added image upload for recipes

--- src/pages/addRecipe/AddRecipe.js
+++ src/pages/addRecipe/AddRecipe.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
+import { getStorage, ref, uploadBytes, deleteObject } from 'firebase/storage';
 import Manager from '../../services/firebase/Manager';
 import { slugify } from '../../services/slugify';
 
@@ -11,10 +12,13 @@ const AddRecipe = () => {
   const [recipeFormData, setRecipeFormData] = useState({
     title: '',
     slug: '',
+    imageName: false,
     category: 'none',
     ingredients: '',
     content: ''
   });
+
+  let fileInputRef = useRef(null);
 
   useEffect(() => {
     let categoryManager = new Manager('categories');
@@ -37,6 +41,39 @@ const AddRecipe = () => {
       title: value,
       slug: slugify(value)
     });
+  };
+
+  const handleImageChange = e => {
+    let file = fileInputRef.current.files[0];
+
+    // if file size is more than 1mo
+    if (file.size > 1024000) {
+      alert(`L'image ne doit pas dépasser 1mo`);
+      return;
+    }
+
+    const storage = getStorage();
+    const recipeImagesFolderName = 'recipe-images';
+
+    // if an image has already been uploaded
+    if (recipeFormData.imageName) {
+      const oldRecipeImageRef = ref(storage, `${recipeImagesFolderName}/${recipeFormData.imageName}`);
+
+      deleteObject(oldRecipeImageRef)
+        .then(() => setRecipeFormData({ ...recipeFormData, imageName: false }))
+        .catch(error => console.error(error));
+    }
+
+    const recipeImageRef = ref(storage, `${recipeImagesFolderName}/${file.name}`);
+
+    uploadBytes(recipeImageRef, file)
+      .then(snapshot => {
+        setRecipeFormData({ ...recipeFormData, imageName: snapshot.metadata.name })
+      })
+      .catch(error => {
+        console.error(error);
+        return;
+      });
   };
 
   const handleSubmit = e => {
@@ -67,14 +104,17 @@ const AddRecipe = () => {
             value={recipeFormData.title}
             onChange={handleTitleChange} />
         </div>
+
         <div>
           <label htmlFor='slug'>Slug</label>
           <input type='text' name='slug' id='slug' required value={recipeFormData.slug} onChange={handleChange} />
         </div>
+
         <div>
           <label htmlFor='image'>Sélectionner une image pour la recette</label>
-          <input type='file' name='image' id='image' />
+          <input type='file' name='image' id='image' ref={fileInputRef} onChange={handleImageChange} />
         </div>
+
         <div>
           <label htmlFor='category'>Choisissez une catégorie</label>
           <select name='category' id='category' required value={recipeFormData.category} onChange={handleChange}>
@@ -84,10 +124,12 @@ const AddRecipe = () => {
             ))}
           </select>
         </div>
+
         <div>
           <label htmlFor='ingredients'>Ingrédients</label>
           <textarea name='ingredients' id='ingredients' required value={recipeFormData.ingredients} onChange={handleChange} />
         </div>
+
         <div>
           <label htmlFor='content' value={recipeFormData.content}>Recette</label>
           <textarea name='content' id='content' required onChange={handleChange} />
