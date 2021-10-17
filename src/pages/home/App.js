3c8779031ca12cee0ae51c647f79736ea62ef0d8
cpatigny <63152989+cpatigny@users.noreply.github.com>
2021-10-17 15:25:10 +0200

    added searchbar

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,18 +1,45 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../providers/UserProvider';
 import { getAuth, signOut } from 'firebase/auth';
 import { RecipesContext } from '../../providers/RecipesProvider';
+import { strContains } from '../../services/strContains';
 
 import { Link } from 'react-router-dom';
 import Loading from '../../components/Loading/Loading';
 import RecipeCard from './RecipeCard';
+import SearchBar from './SearchBar';
+import NothingToShow from '../../components/NothingToShow/NothingToShow';
+
+import noResultFoundImg from '../../assets/img/undraw-lost-online.svg';
 
 import './App.scss';
 
 const App = () => {
 
+  const [recipesToShow, setRecipesToShow] = useState(false);
+  const [search, setSearch] = useState('');
+  const [noSearchResult, setNoSearchResult] = useState(false);
+
   let { user, setUser, userData } = useContext(UserContext);
   let { recipes } = useContext(RecipesContext);
+
+  useEffect(() => {
+
+    if (recipes === 'loading' || recipes === null) return;
+    if (!search) setRecipesToShow(recipes);
+
+    let matchingRecipes = {};
+
+    Object
+      .keys(recipes)
+      .filter(key => strContains(recipes[key].title, search))
+      .forEach(key => matchingRecipes[key] = recipes[key]);
+      
+    if (Object.keys(matchingRecipes).length === 0) matchingRecipes = null;
+
+    setNoSearchResult(matchingRecipes === null);
+    setRecipesToShow(matchingRecipes);
+  }, [recipes, search]);
 
   const handleSignOut = () => {
     const auth = getAuth();
@@ -26,22 +53,33 @@ const App = () => {
 
   return (
     <div className='app container'>
-      <h1>My recipes</h1>
+      <div className='top'>
+        <h1>My recipes</h1>
+        <SearchBar search={search} setSearch={setSearch} />
+      </div>
 
       <div className='wrap'>
         <h2>Liste des recettes</h2>
         { user && <Link to='/add-recipe'>+ Add recipe</Link> }
       </div>
 
-      <div className='recipes'>
-        {Object.keys(recipes).map(key => (
-          <RecipeCard
-            key={key}
-            title={recipes[key].title}
-            imageName={recipes[key].imageName}
-            slug={recipes[key].slug} />
-        ))}
-      </div>
+      { noSearchResult && 
+        <NothingToShow
+          className='no-recipe-to-show'
+          src={noResultFoundImg}
+          message='Aucun résultat ne correspond à votre recherche'
+          alt='no result illustration'
+        />
+      }
+        
+      { recipesToShow && Object.keys(recipesToShow).map(key => (
+        <RecipeCard
+          key={key}
+          title={recipesToShow[key].title}
+          imageName={recipesToShow[key].imageName}
+          slug={recipesToShow[key].slug}
+        />
+      ))}
 
       <footer>
         <p>Made by <span className='name'>Clément</span></p>
---
+++ src/pages/home/SearchBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import './SearchBar.scss';
+
+const SearchBar = ({ search, setSearch }) => {
+
+  const handleChange = e => {
+    let wordToSearch = e.target.value;
+    setSearch(wordToSearch);
+  };
+
+  const reset = () => setSearch('');
+
+  let clearBtn = (
+    <button 
+      className='clear-search' 
+      aria-label='effacer la recherche' 
+      onClick={reset}
+    >
+      <span className='material-icons-round'>clear</span>
+    </button>
+  );
+
+  return (
+    <div className='search-bar'>
+      <input
+        name='search'
+        type='text'
+        autoComplete='off'
+        placeholder='Chercher une recette'
+        aria-label='Chercher une recette'
+        value={search}
+        onChange={handleChange}
+      />
+
+      { search ? clearBtn : <span id='search-icon' className='material-icons-round'>search</span> }
+    </div>
+  );
+}
+
+export default SearchBar;
