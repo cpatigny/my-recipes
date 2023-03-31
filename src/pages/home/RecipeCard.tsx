fcaf2bd3c80874dcd02ae2acc9f10078a5044135
cpatigny <63152989+cpatigny@users.noreply.github.com>
2023-04-01 00:02:45 +0200

    replaced div by link for recipeCard

--- src/pages/home/RecipeCard.tsx
+++ src/pages/home/RecipeCard.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 interface RecipeCardProps {
   title: string;
@@ -7,30 +7,17 @@ interface RecipeCardProps {
 }
 
 const RecipeCard = ({ title, imageName, slug }: RecipeCardProps) => {
-  const navigate = useNavigate();
-
-  const backgroundImageGradient = 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.7))';
-  const backgroundImageUrl = imageName && `https://firebasestorage.googleapis.com/v0/b/my-recipes-5f5d6.appspot.com/o/recipe-images%2F${imageName}?alt=media`;
-
-  const backgroundImage = imageName ? `${backgroundImageGradient}, url(${backgroundImageUrl})` : backgroundImageGradient;
-
-  const openRecipe = () => navigate(`/recette/${slug}`);
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    if (e.key === 'Enter') openRecipe();
-  };
+  const imageUrl = imageName && `https://firebasestorage.googleapis.com/v0/b/my-recipes-5f5d6.appspot.com/o/recipe-images%2F${imageName}?alt=media`;
 
   return (
-    <div
-      onClick={openRecipe}
-      onKeyDown={handleKeyDown}
+    <Link
+      to={`/recette/${slug}`}
       className={`recipe ${imageName ? '' : 'no-image'}`}
-      style={{ backgroundImage }}
-      role='link'
-      tabIndex={0}
     >
-      <h3 className='recipe-title'>{ title }</h3>
-    </div>
+      { imageUrl && <img className='recipe-image' src={imageUrl} alt={title} /> }
+      <div className='gradient' />
+      <p className='recipe-title'>{ title }</p>
+    </Link>
   );
 };
 
