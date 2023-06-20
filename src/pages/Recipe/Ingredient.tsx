7bd748c5619cfb8610c5fa758957a4a7c5625117
cpatigny <63152989+cpatigny@users.noreply.github.com>
2023-06-20 22:49:13 +0200

    replaced homemade checkbox with radix ui one for better accessibility

--- src/pages/Recipe/Ingredient.tsx
+++ src/pages/Recipe/Ingredient.tsx
@@ -3,6 +3,8 @@ import { IngredientsDetails } from '../../types/ingredientDetails';
 import { RecipeIngredientWithId } from '../../types/recipe';
 import { Units } from '../../types/unit';
 
+import * as Checkbox from '@radix-ui/react-checkbox';
+
 interface IngredientProps {
   ingredient: RecipeIngredientWithId;
   ingredientsDetails: IngredientsDetails | null;
@@ -14,9 +16,13 @@ export const Ingredient = ({
   ingredient, ingredientsDetails, units, servingRatio,
 }: IngredientProps) => {
   return (
-    <li>
-      <label className='checkbox-container'>
-        <input type='checkbox' />
+    <li className='checkbox-item'>
+      <Checkbox.Root className='checkbox-root' id={ingredient.id}>
+        <Checkbox.Indicator className='checkbox-indicator'>
+          <div className='checkbox-check-icon'></div>
+        </Checkbox.Indicator>
+      </Checkbox.Root>
+      <label className='checkbox-label' htmlFor={ingredient.id}>
         <IngredientText
           className='checkmark'
           ingredient={ingredient}
