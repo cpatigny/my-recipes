b9b0c3d03669591cb9f667889bd2a05bae641d68
cpatigny <63152989+cpatigny@users.noreply.github.com>
2023-06-30 14:59:28 +0200

    added a function to sign in mocked admin user for tests

---
+++ src/tests-utils/mocks/auth.mock.ts
@@ -0,0 +1,34 @@
+import '../../firebase';
+import { FirebaseError } from 'firebase/app';
+import { signIn } from '../../helpers/auth.helpers';
+import { getAuth } from 'firebase/auth';
+import { get, getDatabase, ref } from 'firebase/database';
+
+export const mockedAdminCredentials = {
+  email: 'admin@example.com',
+  password: 'password',
+} as const;
+
+export const signInWithMockedAdmin = async () => {
+  try {
+    const { email, password } = mockedAdminCredentials;
+    await signIn(email, password);
+  } catch (error) {
+    if (error instanceof FirebaseError && error.code === 'auth/user-not-found') {
+      throw new Error('mockedAdminCredentials is invalid, add the user to firebase authentication emulator or update credentials');
+    }
+  }
+
+  const auth = getAuth();
+  const userId = auth.currentUser?.uid;
+  const db = getDatabase();
+  const userRef = ref(db, `users/${userId}`);
+
+  const snapshot = await get(userRef);
+  const userData = snapshot.val();
+
+  // check if mocked user has admin role
+  if (userData.role !== 'admin') {
+    throw new Error('User is not admin, add the role in database emulator in order for the tests to work');
+  }
+};
