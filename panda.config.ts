bdfb800499d0d87d18bfddb5a45254a74e5a1fb9
cpatigny <63152989+cpatigny@users.noreply.github.com>
2023-09-02 16:32:16 +0200

    trying to fix underline input border rounded on ios (again)

--- panda.config.ts
+++ panda.config.ts
@@ -100,6 +100,7 @@ export default defineConfig({
       color: 'text',
       w: '100%',
       WebkitAppearance: 'none',
+      rounded: '0',
     },
     'h1, h2, h3, h4, h5, h6, label': {
       color: 'text',
