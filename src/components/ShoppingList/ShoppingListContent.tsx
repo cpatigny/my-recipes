b584ac6d0c056773f2a30e454f6750a264a1be40
cpatigny <63152989+cpatigny@users.noreply.github.com>
2024-11-08 19:27:48 +0100

    replace headlessui tabs with react-aria-components one

--- src/components/ShoppingList/ShoppingListContent.tsx
+++ src/components/ShoppingList/ShoppingListContent.tsx
@@ -1,9 +1,9 @@
 import { css, cx } from '../../../styled-system/css';
 import { flex } from '../../../styled-system/patterns';
 import { useShoppingList } from '../../contexts/ShoppingListContext';
-import { shoppingListContainer } from './ShoppingListHeader';
 
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
+import { Tab, TabList, TabPanel, Tabs } from 'react-aria-components';
+import { shoppingListContainer } from './ShoppingListHeader';
 import { ShoppingListIngredientsTab } from './ShoppingListIngredientsTab';
 import { ShoppingListRecipesTab } from './ShoppingListRecipesTab';
 
@@ -13,6 +13,8 @@ const tabStyles = css({
   p: '0.6rem 1rem',
   borderBottom: '2px solid #ddd',
   color: 'text',
+  textAlign: 'center',
+  cursor: 'pointer',
 
   '&[data-selected]': {
     borderColor: 'primary',
@@ -20,34 +22,39 @@ const tabStyles = css({
   },
 });
 
+const tabPanel = cx(
+  shoppingListContainer,
+  // big padding bottom because ShoppingListHeader isn't take into account
+  // in the max height 100% so the content at the bottom is cut
+  css({ py: '1rem 3.5rem', maxH: '100%', overflowY: 'auto' }),
+);
+
 export const ShoppingListContent = () => {
   const { shoppingListRecipes } = useShoppingList();
 
   return (
-    <TabGroup className={flex({ direction: 'column', h: '100%', bg: 'bg' })}>
+    <Tabs
+      aria-label='Votre liste de courses'
+      className={flex({ direction: 'column', h: '100%', bg: 'bg' })}
+    >
       <TabList
         className={flex({
           justify: 'space-between',
         })}
       >
-        <Tab className={tabStyles}>Recettes</Tab>
-        <Tab className={tabStyles}>Ingrédients</Tab>
+        <Tab id='1' className={tabStyles}>
+          Recettes
+        </Tab>
+        <Tab id='2' className={tabStyles}>
+          Ingrédients
+        </Tab>
       </TabList>
-      <TabPanels
-        className={cx(
-          shoppingListContainer,
-          // big padding bottom because ShoppingListHeader isn't take into account
-          // in the max height 100% so the content at the bottom is cut
-          css({ py: '1rem 3.5rem', maxH: '100%', overflowY: 'auto' }),
-        )}
-      >
-        <TabPanel>
-          <ShoppingListRecipesTab recipes={shoppingListRecipes} />
-        </TabPanel>
-        <TabPanel>
-          <ShoppingListIngredientsTab recipes={shoppingListRecipes} />
-        </TabPanel>
-      </TabPanels>
-    </TabGroup>
+      <TabPanel id='1' className={tabPanel}>
+        <ShoppingListRecipesTab recipes={shoppingListRecipes} />
+      </TabPanel>
+      <TabPanel id='2' className={tabPanel}>
+        <ShoppingListIngredientsTab recipes={shoppingListRecipes} />
+      </TabPanel>
+    </Tabs>
   );
 };
