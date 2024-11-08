60f535b6bf0c76672ddc7f7349ee00612e925bef
cpatigny <63152989+cpatigny@users.noreply.github.com>
2024-11-08 23:24:06 +0100

    replace radix-ui checkbox with react-aria-components one

--- src/pages/Recipe/Ingredient.tsx
+++ src/pages/Recipe/Ingredient.tsx
@@ -1,11 +1,10 @@
+import { Checkbox } from 'react-aria-components';
+import { css } from '../../../styled-system/css';
+import { flex, square } from '../../../styled-system/patterns';
 import { IngredientText } from '../../components/IngredientText';
 import { IngredientsDetails } from '../../types/ingredientDetails';
 import { RecipeIngredientWithId } from '../../types/recipe';
 import { Units } from '../../types/unit';
-import { css } from '../../../styled-system/css';
-import { flex, square } from '../../../styled-system/patterns';
-
-import * as Checkbox from '@radix-ui/react-checkbox';
 
 interface IngredientProps {
   ingredient: RecipeIngredientWithId;
@@ -21,65 +20,73 @@ export const Ingredient = ({
   servingRatio,
 }: IngredientProps) => {
   return (
-    <li
-      className={flex({ align: 'center', mb: '1.25rem', fontSize: '1.2rem' })}
-    >
-      <Checkbox.Root
-        id={ingredient.id}
-        className={square({
-          size: '1.125rem',
-          bg: 'transparent',
-          border: '2px solid rgba(0, 0, 0, 0.65)',
-          rounded: '0.1875rem',
-          pos: 'relative',
-          p: '0',
-          _focus: {
-            outline: '2px solid #000',
-            outlineOffset: '3px',
-          },
-          '[data-state="checked"]&': {
-            borderColor: 'transparent',
-          },
-        })}
+    <li>
+      <Checkbox
+        className={flex({ align: 'center', mb: '1.25rem', fontSize: '1.2rem' })}
       >
-        <Checkbox.Indicator
-          className={css({
-            pos: 'absolute',
-            top: '0',
-            bottom: '0',
-            left: '0',
-            right: '0',
-          })}
-        >
-          <div
-            className={css({
-              pos: 'absolute',
-              top: '-3px',
-              left: '3px',
-              h: '1rem',
-              w: '0.5rem',
-              borderWidth: '0 2px 2px 0',
-              borderColor: 'primary',
-              transform: 'rotate(45deg)',
-            })}
-          />
-        </Checkbox.Indicator>
-      </Checkbox.Root>
-      <label
-        htmlFor={ingredient.id}
-        className={css({
-          flex: '1',
-          cursor: 'pointer',
-          pl: '1.125rem',
-        })}
-      >
-        <IngredientText
-          ingredient={ingredient}
-          ingredientsDetails={ingredientsDetails}
-          units={units}
-          servingRatio={servingRatio}
-        />
-      </label>
+        {({ isSelected, isFocused }) => (
+          <>
+            <div
+              data-focused={isFocused}
+              data-selected={isSelected}
+              className={square({
+                size: '1.125rem',
+                bg: 'transparent',
+                border: '2px solid rgba(0, 0, 0, 0.65)',
+                rounded: '0.1875rem',
+                pos: 'relative',
+                p: '0',
+                '[data-focused=true]&': {
+                  outline: '2px solid #000',
+                  outlineOffset: '3px',
+                },
+                '[data-selected=true]&': {
+                  borderColor: 'transparent',
+                },
+              })}
+            >
+              {isSelected && (
+                <div
+                  className={css({
+                    pos: 'absolute',
+                    top: '0',
+                    bottom: '0',
+                    left: '0',
+                    right: '0',
+                  })}
+                >
+                  <div
+                    className={css({
+                      pos: 'absolute',
+                      top: '-3px',
+                      left: '3px',
+                      h: '1rem',
+                      w: '0.5rem',
+                      borderWidth: '0 2px 2px 0',
+                      borderColor: 'primary',
+                      transform: 'rotate(45deg)',
+                    })}
+                  />
+                </div>
+              )}
+            </div>
+            <div
+              className={css({
+                flex: '1',
+                cursor: 'pointer',
+                pl: '1.125rem',
+              })}
+            >
+              <IngredientText
+                ingredient={ingredient}
+                ingredientsDetails={ingredientsDetails}
+                units={units}
+                servingRatio={servingRatio}
+              />
+            </div>
+          </>
+        )}
+      </Checkbox>
     </li>
   );
 };
